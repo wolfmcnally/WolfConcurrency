4b7bee2210ee77a641d5751ff52d8fb5217403e5
Wolf McNally <wolf@wolfmcnally.com>
2019-09-27 15:14:51 -0700

    Added minimum platform versions.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "WolfConcurrency",
+    platforms: [
+        .iOS(.v9), .macOS(.v10_13), .tvOS(.v11)
+    ],
     products: [
         .library(
             name: "WolfConcurrency",
